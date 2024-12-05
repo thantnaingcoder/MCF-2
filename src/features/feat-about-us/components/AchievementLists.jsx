f0e47400b78e4edcda2c5aa38c7fac78c956ef23
sisaung <sisaungvipse780@gmail.com>
2024-12-05 15:43:37 +0630

    change testinomial color and logo marquee gradient

--- src/features/feat-about-us/components/AchievementLists.jsx
+++ src/features/feat-about-us/components/AchievementLists.jsx
@@ -1,6 +1,6 @@
 const AchievementLists = ({ item: { iconUrl, number, name } }) => {
   return (
-    <div className=" sm:justify-start  px-10 sm:px-14 lg:px-0 lg:justify-center col-span-1 flex gap-3 items-center bg-primary-50 py-5">
+    <div className=" sm:justify-start  px-10 sm:px-14 lg:px-0 lg:justify-center col-span-1 flex gap-3 items-center bg-lighterBg py-5">
       <img src={iconUrl} className="size-8" alt="achievement" />
       <div>
         <h1 className="font-heading font-bold text-neutral-900 text-2xl sm:text-3xl sm:leading-10 ">
