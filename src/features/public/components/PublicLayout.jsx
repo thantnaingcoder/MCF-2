de5a993f51283906bc26a3216108e945e06d7b46
sisaung <sisaungvipse780@gmail.com>
2024-12-05 23:37:59 +0630

    add footer

--- src/features/public/components/PublicLayout.jsx
+++ src/features/public/components/PublicLayout.jsx
@@ -1,17 +1,17 @@
 import React, { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
-// import Footer from "./Footer";
+import Footer from "./Footer";
 import PageLoading from "../../../components/PageLoading";
 
 const PublicLayout = () => {
   return (
-    <main className=" flex flex-col h-screen">
+    <main className="flex flex-col h-screen ">
       <Header />
       <Suspense fallback={<PageLoading />}>
         <Outlet />
       </Suspense>
-      {/* <Footer /> */}
+      <Footer />
     </main>
   );
 };
