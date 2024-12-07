4febd02c63b3b5ca9d65a0b2ce575d65ea7767d2
David <lester6586@gmail.com>
2024-12-07 22:09:55 +0630

    fix-vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
-import flowbite from "flowbite/plugin";
-import tailwindcssAnimate from "tailwindcss-animate";
-
 /** @type {import('tailwindcss').Config} */
+
 export default {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+
   theme: {
     fontFamily: {
       sans: ["Hind", "Padauk", "sans-serif"],
-      heading: ["Epilogue", "sans-serif"],
-      label: ["Kite One", "sans-serif"],
     },
     extend: {
       animation: {
@@ -20,24 +17,42 @@ export default {
       },
       keyframes: {
         "infinite-scroll": {
-          from: { transform: "translateX(0)" },
-          to: { transform: "translateX(-100%)" },
+          from: {
+            transform: "translateX(0)",
+          },
+          to: {
+            transform: "translateX(-100%)",
+          },
         },
         border: {
-          to: { "--border-angle": "360deg" },
+          to: {
+            "--border-angle": "360deg",
+          },
         },
         "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
         },
         "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
         },
       },
       backgroundImage: {
         "home-page": "url('./src/assets/aboutUs/heroOther/homePage.png')",
         "course-page": "url('./src/assets/course/heroSection.png')",
+      },
+      fontFamily: {
+        heading: ["Epilogue", "sans-serif"],
+        label: ["Kite One", "sans-serif"],
       },
       colors: {
         lighterBg: "#EFFAF4",
@@ -100,6 +115,5 @@ export default {
       },
     },
   },
-  plugins: [flowbite, tailwindcssAnimate],
+  plugins: [],
 };
-
