b8224a4b43d615cb81f9037fc6089fc6df676ea4
David <lester6586@gmail.com>
2024-12-09 01:53:43 +0630

    test-vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
 
 
-export default {
+module.exports = {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}",flowbite.content()],
 
